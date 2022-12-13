7a62649f47d32748e8ca29147bf83ee5c47dcc8e
DeboraSouza277 <deborasouza277@gmail.com>
2022-12-13 16:33:32 +0000

    sql5

---
+++ 10.FUNCOES STRING.sql
@@ -0,0 +1,29 @@
+-- LEN - retorna a quantidade de caracter
+-- DATALENGHT - retornar q quatidade de caracteres até os espaços
+-- CONCAT 
+-- LEFT
+-- RIGHT
+
+--LEN
+SELECT LEN('Debora Souza    ')
+
+-- DATALENGHT
+SELECT DATALENGTH('Debora Souza    ')
+
+--CONCAT
+
+SELECT 
+	FirstName,
+	LastName,
+	CONCAT(FirstName,' ',LastName) AS 'Nome Completo'
+	
+	FROM DimCustomer
+
+
+-- LEFT
+SELECT LEFT('Debora Souza', 6)
+
+-- RIGHT
+SELECT RIGHT('Debora Souza', 6)
+
+
